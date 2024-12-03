9afb699eb6d8553129dcf6094bec0c1db729e090
Rustypredator <contact@rusty.info>
2024-12-03 10:07:13 +0100

    dynamic api endpoint for debug

---
+++ app/Http/Controllers/Api/Spt/SptController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Spt;
+
+use App\Helpers\SptApiHelper;
+use App\Http\Controllers\Controller;
+
+class SptController extends Controller
+{
+    public function dynamic()
+    {
+        $path = request()->input('path');
+        $response = SptApiHelper::dynamic($path);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Spt\SptController;
 use App\Http\Controllers\Api\Spt\FikaController;
 
 Route::prefix('spt')->group(function () {
     Route::prefix('fika')->group(function () {
         Route::get('/raids', [FikaController::class, 'raids'])->name('api.spt.fika.raids');
     });
+    Route::post('dynamic', [SptController::class, 'dynamic'])->name('api.spt.dynamic');
 });
